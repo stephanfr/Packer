67a6b0b22bc87e4a37faf789a28dac5147f950c6
Stephan Friedl <stephanf01@gmail.com>
2021-01-26 20:47:58 -0700

    Update setup-dev-user.sh

--- RPi/scripts/setup-dev-user.sh
+++ RPi/scripts/setup-dev-user.sh
@@ -7,13 +7,16 @@ if [ ! -z "$DEV_USERNAME" ]; then
     useradd -m -s /bin/bash -p $(mkpasswd --hash=SHA-512 ${DEV_PASSWORD}) ${DEV_USERNAME}
     usermod -aG sudo ${DEV_USERNAME}
 
-    if [ -f "/tmp/uploads/rsa.pub" ]; then
+    if [ -f /tmp/uploads/"$SSH_KEY_FILENAME" ]; then
         mkdir /home/${DEV_USERNAME}/.ssh
-        cp /tmp/uploads/rsa.pub /home/${DEV_USERNAME}/.ssh/authorized_keys
+        cp /tmp/uploads/${SSH_KEY_FILENAME} /home/${DEV_USERNAME}/.ssh/authorized_keys
         chmod -R go= /home/${DEV_USERNAME}/.ssh
         chown -R ${DEV_USERNAME}:${DEV_USERNAME} /home/${DEV_USERNAME}/.ssh
         history -c
     fi
     
+    passwd --expire $DEV_USERNAME  
+
 fi
+
 

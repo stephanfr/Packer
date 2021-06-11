c2b5c8f37dd27161440712656cb8575d774f3cff
Stephan Friedl <stephanf01@gmail.com>
2021-06-11 16:18:26 -0600

    Tweaks for VirtualBox

--- Ubuntu/script/dev_tools_base.sh
+++ Ubuntu/script/dev_tools_base.sh
@@ -11,12 +11,21 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | 
 wget -O - https://download.docker.com/linux/ubuntu/gpg 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/docker.gpg >/dev/null
 apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $UBUNTU_BUILD_NAME stable"
 
-apt-get update
+apt-get update -y
 
 apt-cache policy docker-ce
 
 apt-get install -y docker-ce
 groupadd docker
+
+apt-get install -y build-essential linux-headers-$(uname -r)
+
+if [ $VIRTUALBOX = "true" ]
+then
+apt-get install -y virtualbox-guest-x11
+apt-get install -y virtualbox-guest-utils
+apt-get install -y virtualbox-guest-dkms
+fi
 
 apt-get install -y cmake
 apt-get install -y curl
@@ -32,3 +41,29 @@ apt-get install -y clang-format
 apt-get install -y clang-tidy
 apt-get install -y lcov
 apt-get install -y npm
+
+python3 -m pip install virtualenv
+
+if [ $INSTALL_MINIKUBE = "true" ]
+then
+    cd /tmp
+    wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    cp minikube-linux-amd64 /usr/local/bin/minikube
+    chmod 755 /usr/local/bin/minikube
+
+    curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+    apt-get update -y
+    apt-get install -y kubectl
+    ln -s /bin/kubectl /usr/local/bin/kubectl
+
+    curl https://baltocdn.com/helm/signing.asc | apt-key add -
+    echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+    apt-get update
+    apt-get install helm
+
+    wget -O helmfile_linux_amd64 https://github.com/roboll/helmfile/releases/download/v0.105.0/helmfile_linux_amd64
+    chmod +x helmfile_linux_amd64
+    cp helmfile_linux_amd64 /usr/local/bin/.
+    ln -s /usr/local/bin/helmfile_linux_amd64 /usr/local/bin/helmfile
+fi

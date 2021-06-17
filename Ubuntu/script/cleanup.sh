79c23e91732dd620e0c9dd7cc45021488961152f
Stephan Friedl <stephanf01@gmail.com>
2021-06-16 20:35:37 -0600

    Fixed cleanup for Virtualbox

--- Ubuntu/script/cleanup.sh
+++ Ubuntu/script/cleanup.sh
@@ -2,6 +2,8 @@
 
 apt-get install -y perl
 
+usermod -s /sbin/nologin $SSH_USERNAME
+
 cat << EOF > /opt/cleanupscript.sh
 #!/bin/bash -eu
 crontab -l | grep -v 'cleanupscript.sh' | crontab -
@@ -13,7 +15,7 @@ fi
 EOF
 
 chmod 777 /opt/cleanupscript.sh
-chown root /opt/cleanupscript.sh
+
 crontab -l | { cat; echo "@reboot /bin/bash /opt/cleanupscript.sh"; } | crontab -
 
-usermod -s /sbin/nologin $SSH_USERNAME
+sync
